402ce3dde1706035be6f7cb625170c5563ba9d0c
ritsuri <warit.srisuriyarungrueng@gmail.com>
2022-07-29 13:15:00 +0000

    update inline model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,23 +4,25 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 admin.site.register(CarModel)
-admin.site.register(CarMake)
+
 
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
-    model = CarMake
-    extra = 5
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     fields = ['dealer_id', 'car_type', 'year']
-    inlines = [CarModelInline]
-
+    
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     fields = ['name', 'description']
+    inlines = [CarModelInline]
 
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
 
+#admin.site.register(CarModelAdmin, CarModelInline)
